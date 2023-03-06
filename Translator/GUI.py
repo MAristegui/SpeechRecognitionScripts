e98e650063fa32217470aec6be2732f72951e85c
MAristegui <AristeguiMM@gmail.com>
2023-03-06 11:44:48 -0300

    Bug fix and cleaning code

--- Translator/GUI.py
+++ Translator/GUI.py
@@ -4,15 +4,15 @@ import speech_recognition as sr
 import translator as tr
 
 listener = sr.Recognizer()
-# Función que se ejecuta cuando se inicia la grabación
+
 def start_recording():
     record_button.config(state=tk.DISABLED)
-    origin = var_menu_1.get()
-    dest = var_menu_2.get()
+    origin = var_menu_origin.get()
+    dest = var_menu_dest.get()
     with sr.Microphone() as micro:
             sonido = listener.listen(micro)
             origin_text, dest_text = tr.translateText(origin, dest, sonido)
-    table.insert('','end',values=[origin_text,dest_text]) 
+    table.insert('','end',values=[origin,origin_text,dest,dest_text]) 
     record_button.config(state=tk.NORMAL) 
 
 # Crear gui principal
@@ -25,37 +25,36 @@ marco_menus = tk.Frame(gui, bg="#F0F0F0")
 marco_menus.pack(side="top", pady=10)
 
 # Menus desplegables 
-opciones = tr.getOptions()#["Opción 1", "Opción 2", "Opción 3"]
-var_menu_1 = tk.StringVar()
-var_menu_1.set(opciones[1])
+opciones = list(tr.getOptions())
+opciones.sort()#["Opción 1", "Opción 2", "Opción 3"]
+var_menu_origin = tk.StringVar()
+var_menu_origin.set(opciones[1])
 nombre_menu_1 = tk.Label(marco_menus, text="Idioma origen")
 nombre_menu_1.pack(side="left", padx=10)
-menu_1 = tk.OptionMenu(marco_menus, var_menu_1, *opciones)
+menu_1 = tk.OptionMenu(marco_menus, var_menu_origin, *opciones)
 menu_1.pack(side="left", padx=10)
 
-#opciones_2 = tr.getOptions()#["Opción A", "Opción B", "Opción C"]
-var_menu_2 = tk.StringVar()
-var_menu_2.set(opciones[0])
+var_menu_dest = tk.StringVar()
+var_menu_dest.set(opciones[0])
 nombre_menu_2 = tk.Label(marco_menus, text="Idioma destino")
 nombre_menu_2.pack(side="right", padx=10)
-menu_2 = tk.OptionMenu(marco_menus, var_menu_2, *opciones)
+menu_2 = tk.OptionMenu(marco_menus, var_menu_dest, *opciones)
 menu_2.pack(side="right", padx=10)
 
-# Boton para iniciar grabación
+# Boton grabacion
 record_button = tk.Button(gui, text="Iniciar grabación", command=start_recording)
 record_button.pack(pady=10)
 
-# Tabla 
-table = ttk.Treeview(columns=('Origen','Destino'),show='headings')
-table.heading('Origen', text='Origen')
-table.heading('Destino', text='Destino')
+# Tabla
+table = ttk.Treeview(columns=('Idioma origen','Texto_origin','Idioma destino','Texto_dest'),show='headings')
+table.heading('Idioma origen', text='Idioma origen')
+table.heading('Texto_origin', text='Texto')
+table.heading('Idioma destino', text='Idioma destino')
+table.heading('Texto_dest', text='Texto')
+table.column(0, width=100,anchor='center')
+table.column(1, width=500,anchor='center')
+table.column(2, width=100,anchor='center')
+table.column(3, width=500,anchor='center')
 table.pack()
-
-#nombre_columna_1 = tk.Label(table, text="Idioma origen", bg="#FFFFFF", padx=10, pady=5)
-#nombre_columna_1.grid(row=0, column=0)
-#nombre_columna_2 = tk.Label(tabla, text="Idioma destino", bg="#FFFFFF", padx=10, pady=5)
-#nombre_columna_2.grid(row=0, column=1)
-
-
 
 gui.mainloop()
--- Translator/translator.py
+++ Translator/translator.py
@@ -1,14 +1,21 @@
 import speech_recognition as sr
 from googletrans import Translator
+import googletrans
 
-languages_options = ['Ingles','Español','Frances','Italiano','Aleman']
-language_dictionary = {'Ingles':'en',"Español":'es','Frances':'fr','Aleman':'de','Italiano':'it'}
+language_dictionary = {'Ingles':'en',
+					'Español':'es',
+					'Frances':'fr',
+					'Aleman':'de',
+					'Italiano':'it',
+					'Portugués':'pt',
+					'Turco':'tr'}
 
 translator = Translator()
 listener = sr.Recognizer()
 
+
 def getOptions():
-	return languages_options
+	return language_dictionary.keys()
 
 def translateText(origin, dest, sound):
 	texto = listener.recognize_google(sound, language=language_dictionary.get(origin))
